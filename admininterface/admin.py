58b570a7ef31ed1b55d18d899d0f4a731e659068
Dennis G Vesterlund <dennis.g.vesterlund@atea.se>
2017-01-20 08:10:15 +0100

    Added project to installed apps ;)

--- admininterface/admin.py
+++ admininterface/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.SabConfig)
+class SabConfigAdmin(admin.ModelAdmin):
+    pass
--- admininterface/models.py
+++ admininterface/models.py
@@ -1,20 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-
-
-class User(AbstractBaseUser):
-    username = models.CharField('username', max_length=10, unique=True, db_index=True)
-    email = models.EmailField('email address', unique=True)
-    joined = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default=False)
+from django.contrib.auth.models import User
 
 
 class BaseConnect(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     url = models.URLField()
-    apikey = models.CharField()
+    apikey = models.CharField(max_length=100)
     visible = models.BooleanField()
+
+    def __str__(self):
+        return self.user.__str__() + " - " + self.url
+
+    class Meta:
+        abstract = True
 
 
 class SabConfig(BaseConnect):
